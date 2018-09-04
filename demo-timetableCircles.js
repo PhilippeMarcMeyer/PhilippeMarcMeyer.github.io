2a7fa8e870ea553a7f4594c07fff256b9f1f626e
philippe MEYER <pmg.meyer@gmail.com>
2018-09-04 23:08:07 +0200

    New canvas demo : timetable Circles !

--- demo-timetableCircles.js
+++ demo-timetableCircles.js
@@ -4,22 +4,70 @@ var globals = {
 	mainHeight : 480,
 	mainWidth : 480,
 	radius : 200,
-	timeTable :2,
-	numberPoints : 10
+	timeTable :5,
+	numberPoints : 201,
+	points : [],
+	rotation : 0,
+	color: "#ccccef",
+	bg:"#000"
 };
-
 
  function setup(){
 	var cnv = createCanvas(480, 480);
     cnv.parent('canvasZone');
+	frameRate(5);
+	generate();
 	noFill();
-	stroke(255);
-	noLoop();
+ }
+ 
+ function generate(){
+	 globals.points = [];
+	 globals.rotation = TWO_PI / globals.numberPoints;
+	 for(var i = 0; i < globals.numberPoints; i++){
+		 var rot = i*  globals.rotation;
+		 var x = -cos(rot)*globals.radius;
+		 var y = -sin(rot)*globals.radius;
+		 globals.points.push({x:x,y:y});
+	 }
  }
  
  function draw(){
   translate(width / 2, height / 2); 
-  background(0);
-  ellipse(0, 0, globals.radius*2, globals.radius*2);
+  var newNRPTS = parseInt($("#definition").val());
+  var newTT = parseInt($("#timetable").val());
+  if(newNRPTS != globals.numberPoints){
+	  globals.numberPoints = newNRPTS;
+	  generate();
+  }
+   if(newTT != globals.timeTable){
+	  globals.timeTable = newTT;
+  }
+  var timeTable = globals.timeTable;
+  var numberPoints = globals.numberPoints;
+  var points = globals.points.slice(0);
+  var j;
+
+  background(globals.bg);
+
+  for(var i = 0; i < numberPoints; i++){
+	  j = (i * timeTable) % numberPoints;
+	  var r = 155+i%80;
+	  var g = 155+j%80;
+	  var b = 155+i%47;
+	  stroke(r,g,b);
+	 line(points[i].x,points[i].y,points[j].x,points[j].y);
+  }
+
  }
  
+ $( document ).ready(function() {
+	$("#definition").on("change",function(){
+		$("#definitionInfo").text($(this).val());
+	});	
+	
+	$("#timetable").on("change",function(){
+		$("#timetableInfo").text($(this).val());
+	});				
+});
+ 
+
